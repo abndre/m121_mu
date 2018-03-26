24eec2bfb4b2c0e16b61ccbdecbefa43541c4d32
Andre Santos Barros da Silva <andrezio@live.com>
2018-03-26 09:22:23 -0300

    Update help.js

--- help.js
+++ help.js
@@ -203,3 +203,26 @@ var y = db.movies.aggregate([
         $limit: 1
     },
 ])
+
+#LAB CHAP4
+
+var pipeline = [
+    { $match : { "imdb.rating" : { $gt : 0 },"metacritic" : { $gt : 0 }}},
+    { $facet : { 
+        "topTenImdb" : [        
+            { $sort : { "imdb.rating" : -1 } },
+            { $limit : 10 }
+        ],
+        "topTenMetacritic" : [
+            { $sort : { "metacritic" : -1 } },
+            { $limit : 10 }
+        ]}
+    },
+    { $project : {
+        "commonTopFilms" : {
+            $size : {
+                $setIntersection : [ "$topTenImdb", "$topTenMetacritic" ]
+            }
+        }   
+    }
+}];
